70789e1b88caa348ecd4a47ea743cb8c2f592ae0
aj07mm <aj07mm@hotmail.com>
2019-08-07 11:50:59 -0300

    first

---
+++ isaquebot.py
@@ -0,0 +1,11 @@
+import os
+import telebot
+
+bot = telebot.TeleBot(os.environ.get("ISAQUEBOT_TOKEN"))
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+bot.polling()
