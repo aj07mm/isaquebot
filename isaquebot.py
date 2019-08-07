e79e2c7d7c9b9204c7585b235cdf1cf8117accc4
aj07mm <aj07mm@hotmail.com>
2019-08-07 12:06:49 -0300

    wip

--- isaquebot.py
+++ isaquebot.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import os
 import telebot
 
@@ -6,6 +7,15 @@ bot = telebot.TeleBot(os.environ.get("ISAQUEBOT_TOKEN"))
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
+def handle_messages(messages):
+    for message in messages:
+        if "sergio" in message.text or "Sergio" in message.text:
+            bot.reply_to(message, 'Vamo fuma droga na casa do Sergio')
+        if "maconha" in message.text:
+            bot.reply_to(message, 'Onde?')
+
+bot.set_update_listener(handle_messages)
 
 
 bot.polling()
